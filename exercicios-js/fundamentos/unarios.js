07161eefe463cf378a44867a2da992b9eaf4cae3
RicardoRR29 <RicardoRR29@users.noreply.github.com>
2020-01-28 03:14:09 -0300

    terminada sessao 3 do curso

---
+++ exercicios-js/fundamentos/unarios.js
@@ -0,0 +1,12 @@
+let num1 = 1
+let num2 = 2
+
+
+//não faz diferença ser prefix ou posfix
+num1++
+console.log(num1)
+--num1
+console.log(num1)
+
+console.log(++num1 === num2--)
+console.log(num1 === num2) // o resultado sai falso, ou seja, já não são iguais
