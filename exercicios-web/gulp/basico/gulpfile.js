10445e40b7ee42bf3a4e2c2f81adc121e152919f
RicardoRR29 <r3ifes@gmail.com>
2021-01-14 10:29:59 -0300

    fundamentos do gulp

---
+++ exercicios-web/gulp/basico/gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp')
+const { series, parallel } = require('gulp')
+
+//const series = gulp.series
+
+const antes1 = cb => {
+    console.log('Tarefa Antes 1')
+    return cb()
+}
+
+const antes2 = cb => {
+    console.log('Tarefa Antes 2')
+    return cb()
+}
+
+
+function copiar(cb) {
+    //gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    gulp.src('pastaA/**/*.txt') //  qualquer arquivo dentro da pastaA q tenha extensao .txt 
+        /*
+        .pipe(imagemPelaMetade())
+        .pipe(imagemEmPretoEBranco())
+        .pipe(transformacaoA())       // Pipelining
+        */
+       .pipe(gulp.dest('pastaB'))
+        return cb()
+}
+
+
+const fim = cb => {
+    console.log('Tarefa Fim!')
+    return cb()
+}
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim,
+)
