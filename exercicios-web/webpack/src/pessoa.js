f28ccaa134ebfbbf00f800c37e712d3435449124
RicardoRR29 <r3ifes@gmail.com>
2021-03-19 14:41:17 -0300

    atualizando até a aula 314

--- exercicios-web/webpack/src/pessoa.js
+++ exercicios-web/webpack/src/pessoa.js
@@ -1,3 +1,14 @@
+/*
+module.exports = class Pessoa {
+    cumprimentar() {
+        return 'Bom dia!'
+    }
+}
+*/
+import './modulos/moduloA'
+
 export default class Pessoa {
-    
+    cumprimentar() {
+        return 'Bom dia!'
+    }
 }
